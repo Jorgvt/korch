55b7284aa90b5fa3619ae86e60e45ddadae02ff2
Jorgvt <vila_tronik@hotmail.com>
2022-07-04 21:08:26 +0200

    :sparkles: Added logging during training with a new History object. Closes #2 and #4.

--- korch/_nbdev.py
+++ korch/_nbdev.py
@@ -3,6 +3,7 @@
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"get_pbar_description_from_batch_metrics": "00_core.ipynb",
+         "History": "00_core.ipynb",
          "Module": "00_core.ipynb"}
 
 modules = ["core.py"]
